57bee772d365d3c934446db525f57190d74056be
David Holtz <david.richard.holtz@gmail.com>
2020-09-18 18:51:09 -0400

    remove lambda reference

--- main.tf
+++ main.tf
@@ -49,16 +49,6 @@ data "aws_iam_policy_document" "s3_policy" {
       identifiers = [aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn]
     }
   }
-
-  statement {
-    actions   = ["s3:ListBucket"]
-    resources = ["${aws_s3_bucket.b.arn}"]
-
-    principals {
-      type        = "AWS"
-      identifiers = [aws_iam_role.iam_for_lambda.arn]
-    }
-  }
 }
 
 resource "aws_cloudfront_distribution" "s3_distribution" {
